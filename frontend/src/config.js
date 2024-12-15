3c7f76d7dd947db2368bb4ddfc355ea706dd28a6
pranav-suri-phc <pranav.dev@projecthumancity.com>
2024-12-15 14:50:37 +0530

    http added to urls

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 module.exports.Config = {
-    COQUEST_URL: "3.139.234.197:3010",
-    API_URL: "3.139.234.197:3011",
+    COQUEST_URL: "http://3.139.234.197:3010",
+    API_URL: "http://3.139.234.197:3011",
 };
